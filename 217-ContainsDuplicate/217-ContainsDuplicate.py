9fc493fd30424e9b891a210a1acc95c1876fc0de
Kelechi Opurum <157651060+kelechi055@users.noreply.github.com>
2025-05-04 20:34:51 -0400

    File updated at 5/4/2025, 8:34:51 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.py
+++ 217-ContainsDuplicate/217-ContainsDuplicate.py
@@ -1,19 +1,22 @@
-# Last updated: 5/4/2025, 8:29:23 PM
+# Last updated: 5/4/2025, 8:34:51 PM
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         checked = set()
 
 
-        # [1, 2, 1]
-        # [1]
+        # nums [1, 0, 1]
+        # checked [1, 0, ]
         for i in nums:
             if i in checked:
                 return True
             checked.add(i)
         return False
+        # return false
 
-        #false
-        #false
-        #true
+        # false
+        # false
+        # True
+
+
 
 
