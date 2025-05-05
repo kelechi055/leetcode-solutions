d2396f9577fbf352237a3fefe48a07c7a1615b70
Kelechi Opurum <157651060+kelechi055@users.noreply.github.com>
2025-05-04 20:42:34 -0400

    File updated at 5/4/2025, 8:42:34 PM

--- 242-ValidAnagram/242-ValidAnagram.py
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -1,6 +1,8 @@
-# Last updated: 5/4/2025, 8:41:29 PM
+# Last updated: 5/4/2025, 8:42:34 PM
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if sorted(s) != sorted(t):
+        if len(s) != len(t):
+            return False
+        elif sorted(s) != sorted(t):
             return False
         return True
