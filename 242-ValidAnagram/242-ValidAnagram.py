657b3fb475508257b806474a28781a4856c3daef
kelechi055 <157651060+kelechi055@users.noreply.github.com>
2025-05-11 22:41:12 -0400

    File updated at 5/11/2025, 10:41:10 PM

--- 242-ValidAnagram/242-ValidAnagram.py
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -1,8 +1,36 @@
-# Last updated: 5/4/2025, 8:42:34 PM
+# Last updated: 5/11/2025, 10:41:10 PM
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        elif sorted(s) != sorted(t):
-            return False
+        # initialize an array of 26 elemets to track occurences of letters 'a' - 'z'
+        alphabet = [0] * 26
+
+        # iterate through stirng 's' and increment the letter counts
+        # use the ASCII value to map 'a' to index 0, 'b' to index 1, ..., 'z' to index 25
+
+        for char in s:
+            # make the index the ordinal of each character 'a'
+            index = ord(char) - ord('a')
+            alphabet[index]+=1
+
+        # iterate through stirng 't' and decrement the letter counts
+        for char in t:
+            # make the index the ordinal of each character 'a'
+            index = ord(char) - ord('a')
+            alphabet[index]-=1
+
+        # if all counts are zero, the strings are anagrams
+        for i in alphabet:
+            if i != 0:
+                return False
         return True
+
+        # Time Complexity: O(n)
+
+        # first loop:
+        # subtracts 1 to the index of each letter in string "s"
+
+        # second loop:
+        # subtracts 1 to the index of each letter in string "t"
+
+        # to find out if its an anagram:
+        # if each index = 0, that means stirng "s" and string "t" have thesame letter counts.
